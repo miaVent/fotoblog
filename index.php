d4f7ab0431639ac59188c72fc7abcd5a0a621e6b
guski <mareen.guske@web.de>
2015-11-18 18:55:44 +0000

    initial import

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FOTOBLOG</title>
+        <link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>">
+    </head>
+    <body>
+        <h1>FOTOBLOG</h1>
+    </body>
+</html>
