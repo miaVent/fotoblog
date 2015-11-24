af7f18709468ae1d41a1661d3a090384cd2b798a
guski <mareen.guske@web.de>
2015-11-24 17:08:37 +0000

    new link features

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <body> <!- describes the visible page content --> 
         <h1>mareen-guske</h1> <!- you can use 6 headlines, sollte auch nur für Überschriften genutzt werden --> 
         <h2>NIKON D5000</h2>
-        <a href="http://www.fotoblog.mareen-guske.de">Alter Fotoblog mareen-guske</a> <!- benötigt immer einen Beschreibung vom Link auf diese wird dann geklickt -->
-        <p title="Irland Bray"><img src="Irland_Bray_22082013-1-ba.jpg" width="1000"  height="500"></p> <!- Grössenangaben sind immer in Pixel, Einbingund von Bildern, durch das Attribut title wurde ein tooltip hinzugefügt (Information zum Bild) -->
+        <a href="http://www.fotoblog.mareen-guske.de" target="_blank">Alter Fotoblog mareen-guske</a> <!- benötigt immer einen Beschreibung/Symbol (visueller Teil) vom Link auf diese wird dann geklickt -->
+        <p title="Mareen mit Fotoaparat"><img src="Olympus-Elisa-10092015-26.jpg" width="1000"  height="500"></p> <!- Grössenangaben sind immer in Pixel, Einbingund von Bildern, durch das Attribut title wurde ein tooltip hinzugefügt (Information zum Bild) -->
         <h3>Über Mareen Guske</h3>
         <p id="p01">Ich fotografiere seit 3 Jahren und versuche gerade meinen Fotoblog selber in HTML zu gestalten.</p> <!- Browsers automatically add an empty line before and after a paragraph. / Use id to address single elements -->
         <p>Mein Equipment ist: <hr> eine NIKON D5000 mit einem Nikon Kit-Objektiv, einem Sigma Zoomobjektiv und einem Tamron Festbrennweitenobjektiv</p> <!- es wird eine horizontale Linie erzeugt durch das hr tag -->
@@ -18,10 +18,15 @@ Während der schönen und warmen Herbsttage habe ich sehr viele Aufnahmen drauß
 Aktuell schaue ich sehr viele YouTube Videos.</pre> <!- The text inside the pre element is displayed in a fixed-width font (usually Courier), and it preserves both spaces and line breaks. -->
         <p class="error">Seit September 2015 besuche ich einen Fotokurs bei dem Fotografen Werner Bauer in Dachau. Die Hausaufgaben von dem Kurs stellen mich immer wieder vo neue Herausforderungen.</p>
         <p class="error">Durch die Hausaufgaben wird meine Kreativität aktiviert, die bis jetzt leider sehr wenig zum tragen kam.</p> <!- Use class to address groups of elements. -->
+        <p>Aktuell schaue ich sehr viele Videos auf YouTube. Besonders die ganzen Videos von <a href="https://www.youtube.com/user/jaworskyjpictures" target="_blank"</a>Benjamin Jaworskyj</a> Fotografie.</p> <!- das target Attribute öffnet den link in einem neuem Tab oder Browser Fenster -->
         </p><adress>
 Mareen Guske<br>
 Radlkoferstr. 11<br>
 81373 München<br>
         </adress> <!- defines contact information (author, owner), The adress element is usually displayed in italic. Most browsers will add a line break before and after the element. -->
+        <p>Klicke auf das Bild und Du gelangst zum Fotoblog von Mareen Guske.</p> 
+        <a href="http://www.fotoblog.mareen-guske.de" target="_blank">
+            <img src="fotografierend_0005.gif" alt="Blog Mareen Guske">
+        </a>
     </body>
 </html>
